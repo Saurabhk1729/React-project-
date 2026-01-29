5c954bc8288fdfbb004e0cfc589b627e9fb442b5
Saurabhk1729 <saurabhkumar1729e@gmail.com>
2026-01-29 08:58:25 +0530

    basic ui of useState

--- 06-uiproject/src/components/Section1/RightContent.jsx
+++ 06-uiproject/src/components/Section1/RightContent.jsx
@@ -4,7 +4,7 @@ import RightCard from './RightCard'
 
 const RightContent = (props) => {
   return (
-    <div id='right' className='h-full w-190  py-3 px-4 flex gap-3 overflow-x-auto flex-nowrap'>
+    <div className='h-full w-190  py-3 px-4 flex gap-3 overflow-x-auto flex-nowrap'>
       {props.users.map(function(elem,idx){
         return <RightCard key={idx} id={idx} img={elem.img} tag={elem.tag}/>
       })}
